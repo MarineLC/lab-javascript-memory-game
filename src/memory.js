e4ef1f708fe769a88356b95e3e20a3ac864e7286
Marine Le Cleach <lecleach.m@gmail.com>
2023-11-29 15:02:41 +0100

    iteration 2.2.3

--- src/memory.js
+++ src/memory.js
@@ -38,5 +38,11 @@ class MemoryGame {
 
   checkIfFinished() {
     // ... write your code here
+    
+    if(this.pairsGuessed == (this.cards.length)/2){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
