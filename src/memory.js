bc9a9c0b8d6cdb4ed05fbf3edcfaed13076541f8
Marine Le Cleach <lecleach.m@gmail.com>
2023-11-29 14:48:55 +0100

    iteration 2.2.1

--- src/memory.js
+++ src/memory.js
@@ -7,8 +7,22 @@ class MemoryGame {
     this.pairsGuessed = 0; 
   }
 
-  shuffleCards() {
+  shuffleCards(arr) {
     // ... write your code here
+   if (!this.cards) {
+      return undefined;
+    };
+
+    let i = this.cards.length;
+  
+    while(--i > 0){
+      let temp = this.cards[i];
+      let randIndex = Math.floor(Math.random() * i);
+     this.cards[i] =  this.cards[randIndex];
+     this.cards[randIndex] = temp;
+  }
+  return this.cards; 
+
   }
 
   checkIfPair(card1, card2) {
